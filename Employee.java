86b3fe810acb8a60348508af9aa87cc14dfbfd34
dileep9848 <60995942+dileep9848@users.noreply.github.com>
2020-08-07 15:55:06 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,28 @@
+
+
+
+public class Employee
+{
+   private int empId;
+   private String empName;
+   
+   public void setEmpId(int empId)
+   {
+       this.empId=empId;
+   }
+   
+   public void setEmpName(String empName)
+   {
+      this.empName=empName;
+   }
+   
+   public int getEmpId()
+   {
+        retrun empId;
+   }
+   
+   public String getEmpName()
+   {
+       retrun empName;
+    }
+}
